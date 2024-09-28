92477b327f69f303d88ab3b24d49c6bab3015476
stackgen-automation <automation@stackgen.com>
2024-09-28 09:56:08 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -23,14 +23,20 @@ output "aws_lambda_hello_kitty_function_lokeshdingra14_invoke_arn" {
 }
 
 
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_arn" {
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_1405_arn" {
   value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
   sensitive = false
 }
 
 
-output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_name" {
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_1405_name" {
   value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
+  sensitive = false
+}
+
+
+output "aws_s3_lokeshdingra14_bucket_name" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
   sensitive = false
 }
 
@@ -53,19 +59,13 @@ output "aws_s3_lokeshdingra14_arn" {
 }
 
 
-output "aws_s3_lokeshdingra14_bucket_name" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
-  sensitive = false
-}
-
-
-output "aws_iam_role_hello_kitty_role_arn" {
+output "aws_iam_role_hello_kitty_role_lokeshdingra14_arn" {
   value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
   sensitive = false
 }
 
 
-output "aws_iam_role_hello_kitty_role_name" {
+output "aws_iam_role_hello_kitty_role_lokeshdingra14_name" {
   value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
   sensitive = false
 }
